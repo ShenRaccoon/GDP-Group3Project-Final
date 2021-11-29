a4f6cb4123c5e96f1e33ad4877e3edb8581f4cf0
ShenRaccoon <93973684+ShenRaccoon@users.noreply.github.com>
2021-11-29 15:06:27 +0800

    a

--- Group3-Project-final/Assets/Script/EmailShuffle.cs
+++ Group3-Project-final/Assets/Script/EmailShuffle.cs
@@ -6,21 +6,23 @@ public class EmailShuffle
 {
     ArrayList emailTitle = new ArrayList()
     {
-
+        "this"
     };
     ArrayList emailDescription = new ArrayList()
     {
-
+        "that"
     };
     ArrayList emailLink = new ArrayList()
     {
-
+        "yes"
     };
 
     public string Shuffle()
     {
         string combinedString = (string)emailTitle[Random.Range(0, emailTitle.Count)];
+        combinedString += "\n";  
         combinedString += (string)emailDescription[Random.Range(0, emailDescription.Count)];
+        combinedString += "\n";
         combinedString += (string)emailLink[Random.Range(0, emailLink.Count)];
         return combinedString;
     }
